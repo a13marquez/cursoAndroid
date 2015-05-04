c1c80756b055148003d59875933167250f0ea91c
Álvaro Márquez <marquezalvaro13@gmail.com>
2015-05-04 17:46:13 +0200

    Button Method 3

---
+++ ButtonMethod3/app/src/main/java/com/buttonmethod3/amarquez/buttonmethod3/MainActivity.java
@@ -0,0 +1,27 @@
+package com.buttonmethod3.amarquez.buttonmethod3;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void performStuff(View v){
+        switch (v.getId()){
+            case R.id.button1:
+                Log.d("a13marquez", "Button1 clicked");
+                break;
+            case R.id.button2:
+                Log.d("a13marquez", "Button1 clicked");
+                break;
+        }
+    }
+}
